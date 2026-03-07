1656a8fda7917a9a95a57660ca65a56d596c8bcf
judithmovillat <judithmovilla1gmail.com>
2026-03-07 20:14:03 +0100

    remove router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Trayectory from "./components/Trayectory";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 
-const App = () => {
-
+function App() {
   return (
     <div className="bg-darkBlack text-lightWhite min-h-screen">
       <Navbar />
@@ -20,6 +12,6 @@ const App = () => {
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
