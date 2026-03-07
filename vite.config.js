7c647948118c24095b81cb62c0569c604029afc2
judithmovillat <judithmovilla1gmail.com>
2026-03-07 20:18:26 +0100

    remove router

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto es clave para que los assets funcionen en Vercel
+  base: "./", // Crucial para despliegue en Vercel
 });
