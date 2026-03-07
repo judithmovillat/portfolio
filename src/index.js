6f0cef1e9c1cee1e2cc285c115f307ea02500211
judithmovillat <judithmovilla1gmail.com>
2026-03-07 19:05:36 +0100

    Build listo para Vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
