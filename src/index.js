efeee45e4423092963cdac723a9ae01bb9c0bcc8
judithmovillat <judithmovilla1gmail.com>
2026-03-09 18:10:18 +0100

    remove router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const root = ReactDOM.render(document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
   <React.StrictMode>
     <App />
