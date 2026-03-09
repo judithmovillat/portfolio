5bce335cb3f7af2d61d9fa431e33d73ba0247f28
judithmovillat <judithmovilla1gmail.com>
2026-03-09 18:49:29 +0100

    remove router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Trayectory from "./components/Trayectory";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
